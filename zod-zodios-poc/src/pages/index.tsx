2321e56d6eefa67fb9e2fde5af1091c25a827bad
Carolina Ferreira <carolina.ferreira@poatek.com>
2024-10-02 16:09:38 -0300

    tailwindcss config

--- zod-zodios-poc/src/pages/index.tsx
+++ zod-zodios-poc/src/pages/index.tsx
@@ -1,13 +1,5 @@
 import RegisterForm from "@/components/registerForm";
 
 export default function Home() {
-  return (
-    <div>
-      <h1>Welcome to Zodios</h1>
-      <p>
-        This is a proof of concept for using Zod to validate forms in a Next.js app.
-      </p>
-      <RegisterForm />
-    </div>
-  );
+	return <RegisterForm />;
 }
